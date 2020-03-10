34c8fdf184fdedbcc565417ad23a33dbb13f1c3c
JohnDorian00 <32276068+JohnDorian00@users.noreply.github.com>
2020-03-10 04:23:34 +0300

    add external connection

---
+++ js/my_script.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+let msg = 'Привет'
+	, age = 25
+	, user = "remai";
+
+
+document.cookie = "user=remai";
+//document.cookie = "msg=" + encodeURIComponent(msg);
+
+
+console.log( document.cookie );
+
+
+
+// alert(my_var);
+// alert(age);
+// alert(`Привет, ${user}, № ${age}`);
